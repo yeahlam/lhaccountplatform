33ebd317fa244cf55a4fd6967b531f9d40a2ea7e
guanyinghua <972989152@qq.com>
2019-07-18 00:24:18 +0800

    我要举报

--- src/components/report.vue
+++ src/components/report.vue
@@ -12,7 +12,7 @@
             </div>
         </div>
         <div class="photo">
-            <div class="clearfix">
+            <div class="photo-list">
                 <div class="photo-row">
                     <img class="picture" src="../assets/add_picture.png">
                     <img class="close-right" src="../assets/close_right.png">
@@ -34,8 +34,18 @@
                     <img class="close-right" src="../assets/close_right.png">
                 </div>
             </div>
+            <div class="count">[0/5]</div>
         </div>
-
+        <div class="tips">*点击“+”添加图片，点击“×”可删除图片，最多上传5张图片</div>
+        <div class="location">
+            <div class="text">点击右侧按钮进入地图进行定位</div>
+            <div class="get">定位</div>
+        </div>
+        <div class="tips">*请详细描述方案发生位置，以便我们更快地核实处理案件</div>
+        <div class="phone">
+            <input class="text" placeholder="请填写您的手机号码以便反馈进度"></input>
+        </div>
+        <div class="tips">*联系方式将严格保密</div>
     </div>
 </template>
 
@@ -105,17 +115,22 @@
             font-size 0.28rem
             line-height 0.3rem
             padding-left 0.14rem
-
+    .photo-row:last-child
+        margin-right 0
     .photo
         background-color #fff
         width 100%
-
+        padding-top 0.3rem
+    .photo-list
+        width calc(100% - 0.28rem)
+        overflow hidden
+        margin 0 auto
     .photo-row
         position relative
         width 1.2rem
         height 1.64rem
+        margin-right calc((100% - 6rem)/4)
         float left
-
         img.picture
             display block
             width 1.2rem
@@ -131,10 +146,64 @@
             position absolute
             top 0
             right 0
-.clearfix:after
-    clear both
-.clearfix
-    zoom 1
+    .count
+        text-align right
+        padding 0.48rem 0.2rem 0.12rem 0
+        font-size 0.28rem
+        color #666
+    .tips
+        font-size 0.24rem
+        color #808080
+        line-height 0.24rem
+        padding 0.2rem 0rem 0.3rem 0.12rem
+        text-align left
+    .location
+        background-color #fff
+        height 1.1rem
+        display flex
+        align-items center
+        justify-content space-between
+        width 100%
+        padding 0 1rem 0 0.12rem
+        box-sizing border-box
+        .text
+            font-size 0.32rem
+            color #282828
+            background url("../assets/location.png") no-repeat left center
+            background-size 0.3rem 0.4rem
+            height 0.42rem
+            line-height 0.42rem
+            padding-left 0.42rem
+            color #282828
+        .get
+            line-height 0.42rem
+            height 0.42rem
+            font-size 0.32rem
+            color #2881e5
+    .phone
+        background-color #fff
+        height 1.1rem
+        width 100%
+        padding 0 1rem 0 0.12rem
+        box-sizing border-box
+        display flex
+        align-items center
+        .text
+            font-size 0.32rem
+            color #282828
+            background url("../assets/location.png") no-repeat left center
+            background-size 0.3rem 0.4rem
+            height 0.42rem
+            line-height 0.42rem
+            padding-left 0.42rem
+            display block
+            border 0
+            outline none
+            color #282828
+
+
+
+
 
 
 
