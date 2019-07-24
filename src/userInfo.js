2490566ba3c911e5f121d68e0fd0d72bfc1bb59e
412319003@qq.com <you@example.com>
2019-07-25 00:29:14 +0800

    屏蔽Login逻辑

--- src/userInfo.js
+++ src/userInfo.js
@@ -13,8 +13,8 @@ export async function getUserDict() {
 }
 
 export async function login() {
-    let res = await http.get(urls.login)
-    if (res.data.data == -1) {
-        window.location.href='https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx7274b4cf11e4229a&redirect_uri=http%3A%2F%2F20q12h9711.iok.la%2Fwx%2Fbasic-info%2Fuser&response_type=code&scope=snsapi_base&state=#wechat_redirect'
-    }
+    // let res = await http.get(urls.login)
+    // if (res.data.data == -1) {
+    //     window.location.href='https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx7274b4cf11e4229a&redirect_uri=http%3A%2F%2F20q12h9711.iok.la%2Fwx%2Fbasic-info%2Fuser&response_type=code&scope=snsapi_base&state=#wechat_redirect'
+    // }
 }
